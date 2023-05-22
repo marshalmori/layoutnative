673751ebe6a7b9ae450b6a3a7ba647c66dd5cd78
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-05-22 16:11:00 -0300

    Alinhando os conteúdos com flex box

--- App.js
+++ App.js
@@ -17,11 +17,25 @@ const styles = StyleSheet.create({
   contenedor: {
     backgroundColor: 'cornflowerblue',
     flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
-  caja1: {backgroundColor: 'navy', padding: 20, flex: 1},
-  caja2: {backgroundColor: 'yellow', padding: 20, flex: 1},
-  caja3: {backgroundColor: 'green', padding: 20, flex: 1},
-  caja4: {backgroundColor: 'teal', padding: 20, flex: 3},
+  caja1: {
+    backgroundColor: 'navy',
+    padding: 20,
+  },
+  caja2: {
+    backgroundColor: 'yellow',
+    padding: 20,
+  },
+  caja3: {
+    backgroundColor: 'green',
+    padding: 20,
+  },
+  caja4: {
+    backgroundColor: 'teal',
+    padding: 20,
+  },
 });
 
 export default App;
