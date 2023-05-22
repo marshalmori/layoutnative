9158800799d72fdd42b8b787fa161afbc294fc51
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-05-22 15:34:47 -0300

    Mostrar as diferentes direções do Flex box

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 const App = () => {
-  return <View></View>;
+  return (
+    <View style={styles.contenedor}>
+      <View style={styles.caja1}></View>
+      <View style={styles.caja2}></View>
+      <View style={styles.caja3}></View>
+      <View style={styles.caja4}></View>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  contenedor: {
+    backgroundColor: 'cornflowerblue',
+    flex: 1,
+  },
+  caja1: {backgroundColor: 'navy', padding: 20},
+  caja2: {backgroundColor: 'yellow', padding: 20},
+  caja3: {backgroundColor: 'green', padding: 20},
+  caja4: {backgroundColor: 'teal', padding: 20},
+});
 
 export default App;
